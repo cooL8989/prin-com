7f11d4d049be6be0cb30b9e6503de03289b8c40f
DanielTitan <118362748+cooL8989@users.noreply.github.com>
2023-05-20 14:44:23 +0100

    Update contact.php

--- nous-joindre/contact.php
+++ nous-joindre/contact.php
@@ -9,7 +9,7 @@ $to = "voundiasseng@gmail.com";
 $subject = "Mail From TMS";
 
 $txt ="Name = ". $name . "\r\n  Company = " . $company . "\r\n Email =" . $email . "\r\n Phone =" . $phone . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" ."CC: somebodyelse@example.com";
+$headers = " " . "\r\n" ."CC: nn";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }

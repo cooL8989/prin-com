0e88e9605d959549384375c6c17d34165f005e46
DanielTitan <118362748+cooL8989@users.noreply.github.com>
2023-05-20 15:20:47 +0100

    Update contact.php

--- nous-joindre/contact.php
+++ nous-joindre/contact.php
@@ -1,18 +1,334 @@
-<?php
-//get data from form  
-$name = $_POST['Name'];
-$company = $_POST['Company'];
-$email = $_POST['Email'];
-$phone = $_POST['Phone'];
-$message = $_POST['Message'];
-$to = "voundiasseng@gmail.com";
-$subject = "Mail From TMS";
+<!DOCTYPE html>
+<html data-wf-domain="www.provencecanadalab.ca" data-wf-page="6074583e03881b79fa8085a9"
+	data-wf-site="6074583e03881b082780857b" class=" w-mod-js">
 
-$txt ="Name = ". $name . "\r\n  Company = " . $company . "\r\n Email =" . $email . "\r\n Phone =" . $phone . "\r\n Message =" . $message;
-$headers = " " . "\r\n" ."CC: nn";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:./index.html");
-?>
+<head>
+	<meta charset="utf-8">
+	<title>Nous joindre | Laboratoire Provence Canada</title>
+	<meta
+		content="Give your new startup a leg-up with this fresh UI kit that includes everything you’ll need to launch in style."
+		name="description">
+	<meta content="Nous joindre | Laboratoire Provence Canada" property="og:title">
+	<meta
+		content="Give your new startup a leg-up with this fresh UI kit that includes everything you’ll need to launch in style."
+		property="og:description">
+	<meta content="Nous joindre | Laboratoire Provence Canada" property="twitter:title">
+	<meta
+		content="Give your new startup a leg-up with this fresh UI kit that includes everything you’ll need to launch in style."
+		property="twitter:description">
+	<meta property="og:type" content="website">
+	<meta content="summary_large_image" name="twitter:card">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<link href="../css/laboratoire-provence.739a0ad22.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com" rel="preconnect">
+	<link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+	<script src="../js/webfont.js" type="text/javascript"></script>
+	<script
+		type="text/javascript">WebFont.load({ google: { families: ["Work Sans:100,200,300,regular,500,600,700,800,900"] } });</script>
+	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif]-->
+	<script
+		type="text/javascript">!function (o, c) { var n = c.documentElement, t = " w-mod-"; n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch") }(window, document);</script>
+	<link href="../images/l1.png" rel="shortcut icon" type="image/x-icon">
+	<link href="../images/l2.png" rel="apple-touch-icon">
+	<style>
+		body {
+			-moz-osx-font-smoothing: grayscale;
+			-webkit-font-smoothing: antialiased;
+		}
+
+		select,
+		input,
+		textarea {
+			-webkit-appearance: none;
+		}
+
+
+		.weglot-container.wg-default.weglot-container--left {
+			display: none !important;
+		}
+	</style>
+</head>
+
+<body>
+	<div id="Top" class="back-to-top-container"><a href="#Top" class="back-to-top-button w-inline-block"><img
+				src="../fonts/6099525e5509c8d55196eabf_Arrow%206.svg" loading="lazy" width="16" alt class="image"></a>
+	</div>
+	<div class="top-bar">
+		<div class="main-container">
+			<div class="top-bar-contents">
+				<div class="top-bar-item"><img src="../fonts/6074583e03881bf168808632_icon-location-white.svg" alt
+						class="top-bar-item-icon"><a href="#" class="text-white small-text">Laboratoire Provence Canada,
+						675 rue Bergeron, Drummondville, J2C 7V5 (Québec) Canada<br></a></div>
+				<div class="top-bar-items-wrapper">
+					<div class="top-bar-item"><img src="../fonts/6074583e03881bc39d808642_icon-phone-white.svg" alt
+							class="top-bar-item-icon"><a href="#" class="text-white small-text">(819) 850-1536</a></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="navbar-wrapper sticky-top">
+		<div class="main-container">
+			<div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease"
+				data-easing2="ease" role="banner" class="navbar w-nav">
+				<div class="navbar-row">
+					<div class="navbar-left-contents"><a href="/" class="brand adjacent-to-nav-menu w-nav-brand"><img
+								src="../images/60746b40f41171d7852ed898_Layer%201.png" loading="eager" width="150"
+								sizes="(max-width: 479px) 40vw, (max-width: 767px) 23vw, (max-width: 991px) 150px, 14vw"
+								srcset="../images/60746b40f41171d7852ed898_Layer%201-p-500.png 500w, ../images/60746b40f41171d7852ed898_Layer%201-p-800.png 800w, ../images/60746b40f41171d7852ed898_Layer%201.png 1080w"
+								alt class="brand-image"></a>
+						<nav role="navigation" class="nav-menu w-nav-menu"><a href="../a-propos/a-propos.html" class="w-inline-block">
+								<div class="text-block-5">À PROPOS</div>
+							</a><a href="../services/services.html" class="w-inline-block">
+								<div class="text-block-6">SERVICES</div>
+							</a><a href="../produits/produits.html" class="w-inline-block">
+								<div class="text-block-7">PRODUITS</div>
+							</a><a href="../certifications/certifications.html" class="w-inline-block">
+								<div class="text-block-7">CERTIFICATIONS</div>
+							</a></nav>
+					</div>
+					<div class="navbar-functions">
+						<div class="hide-on-mobile"><a href="nous-joindre.html" aria-current="page"
+								class="button-copy small w-inline-block w--current">
+								<div class="text-block-8">NOUS JOINDRE</div>
+							</a></div>
+						<div class="navbar-functions-divider"></div>
+						<div class="menu-button w-nav-button"><img
+								src="../fonts/6074583e03881b2d9d808619_icon-menu-dark.svg" loading="eager" alt
+								class="menu-button-image"></div>
+						<div data-w-id="974ea470-e9f6-5bcd-b3b0-d0197ee74363" class="wg-element-wrapper sw5">
+							<div data-hover="true" data-delay="300" data-w-id="974ea470-e9f6-5bcd-b3b0-d0197ee74364"
+								class="wg-dropdown-1 w-dropdown">
+								<div lang="en" class="wg-dd-1-togle w-dropdown-toggle">
+									<div class="text-block-46">en</div>
+								</div>
+								<nav data-w-id="974ea470-e9f6-5bcd-b3b0-d0197ee74368"
+									class="wg-dd-1-list w-dropdown-list"><a href="#" lang="fr"
+										class="wg-dropdown-1-link w-dropdown-link">fr</a></nav>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div style="opacity:0" class="load-first">
+		<div class="image-bloc-right-contact wf-section">
+			<div class="main-container">
+				<div class="w-layout-grid grid-halves">
+					<div class="container"><img src="../images/60a6b2126c08d80c37588ed8_iStock-1221186547%201.jpg"
+							srcset="../images/60a6b2126c08d80c37588ed8_iStock-1221186547%201-p-800.jpeg 800w, ../images/60a6b2126c08d80c37588ed8_iStock-1221186547%201-p-1080.jpeg 1080w, ../images/60a6b2126c08d80c37588ed8_iStock-1221186547%201.jpg 1400w"
+							sizes="(max-width: 479px) 52vw, (max-width: 767px) 88vw, 600px" alt class="image-4"></div>
+					<div id="w-node-_717b09c8-dab7-ed1d-cfda-d79ebfe7c394-fa8085a9" class="contact-section">
+						<div class="div-block-9">
+							<h1 class="medium-heading-serrvice-2-copy-copy"><strong>Nous joindre</strong></h1>
+							<p class="texte-top-about-us">Pour toute demande, n’hésitez pas à nous contacter par
+								courriel, téléphone ou courrier</p>
+							<div class="w-layout-grid contact-methods">
+								<div>
+									<h6 class="sans-serif no-bottom-margin">Laboratoire Provence Canada</h6>
+									<div class="text-block-9">5925 Avenue Monkland, suite 202<br>Montréal, QC
+										Canada<br>H4A 1G7</div>
+								</div>
+								<div class="div-block-4 w-clearfix">
+									<h6 class="sans-serif no-bottom-margin">Contact</h6>
+									<div class="text-block-10">514-843-3883</div>
+									<a href="mailto:voundiasseng@gmail.com" class="link-3"><strong><em
+												class="italic-text">info@provencecanadalab.ca</em></strong></a>
+								</div>
+							</div>
+							<div class="w-layout-grid contact-methods">
+								<div>
+									<h6 class="sans-serif no-bottom-margin">Filiale exclusive Asie-Pacifique:</h6>
+									<div class="text-block-9">Société Blue Skin Asia Limited. <br>Central, Hong Kong
+										SAR.</div>
+								</div>
+								<div class="div-block-4 w-clearfix">
+									<h6 class="sans-serif no-bottom-margin">Contact</h6>
+									<div class="text-block-10">Guillaume Barnouin<br>Directeur des ventes</div><a
+										href="mailto:voundiasseng@gmail.com" class="link-3"><strong><em
+												class="italic-text">Info.asia@blue-skin.asia</em></strong></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section-large bg-match-text wf-section">
+			<div class="main-container">
+				<div class="w-layout-grid grid-halves align-content-center">
+					<div class="container">
+						<h3 class="no-bottom-margin">Dites-nous <br>en plus !</h3>
+					</div>
+					<div class="container">
+
+						<div class="form-block w-form">
+                                    <?php
+                                    //get data from form 
+                            
+                            if(!empty($_POST["send"])){
+                                    $name = $_POST['Name'];
+                                    $company = $_POST['Company'];
+                                    $email = $_POST['Email'];
+                                    $phone = $_POST['Phone'];
+                                    $message = $_POST['Message'];
+                                    $to = "voundiasseng@gmail.com";
+                                    $subject = "Mail From TMS";
+                                    $txt ="Name = ". $name . "\r\n  Company = " . $company . "\r\n Email =" . $email . "\r\n Phone =" . $phone . "\r\n Message =" . $message;
+                                
+                            
+                                    if(mail($to,$name, $txt)){
+                                        $mes = "recu";
+                                    }
+                            }
+                           
+                                    ?>
+							<form id="wf-form-Formulaire-de-contact" name="wf-form-Formulaire-de-contact"
+								data-name="Formulaire de contact"  action= "contact.php" method="post" class="form-grid-vertical">
+								
+								<input type="text" class="form-input white w-input" maxlength="256" name="Name"
+									data-name="Name" placeholder="Nom*" id="Name" required>
+									<input type="text"
+									class="form-input white w-input" maxlength="256" name="Company" data-name="Company"
+									placeholder="Compagnie" id="Company">
+									<input type="email"
+									class="form-input white w-input" maxlength="256" name="Email" data-name="Email"
+									placeholder="Courriel*" id="Email" required>
+									<input type="tel"
+									class="form-input white w-input" maxlength="256" name="Phone" data-name="Phone"
+									placeholder="Téléphone*" id="Phone-2" required>
+									<input type="text"
+									class="form-input white w-input" maxlength="256" name="Message" data-name="Message"
+									placeholder="Message*" id="Message" required>
+									<input type="submit" value="Envoyer" name="send" data-wait="S'il vous plaît, attendez..."id="w-node-d1bb963c-184b-c68c-b957-42c4ed4a00df-fa8085a9" class="button w-button">
+								<div class="small-text text-white">* Champs obligatoire</div>
+							</form>
+
+							<div class="form-success w-form-done">
+								<div>Merci! Votre requête a été reçue!</div>
+							</div>
+							<div class="form-error w-form-fail">
+								<div>Oops! Une erreur s'est produite lors de la soumission du formulaire.</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div><img src="../fonts/6074583e03881b4be0808659_pattern-2.svg" alt class="bg-radial">
+		</div>
+	</div>
+	<div style="opacity:0" class="load-second">
+		<div class="footer wf-section">
+			<div class="main-container narrow">
+				<div class="footer-top-contents">
+					<div class="container-small footer-text"><a href="#" class="footer-logo-link w-inline-block"><img
+								src="../images/60746c0507fbbbfc7234d80d_Neg.png"
+								srcset="../images/60746c0507fbbbfc7234d80d_Neg-p-500.png 500w, ../images/60746c0507fbbbfc7234d80d_Neg-p-800.png 800w, ../images/60746c0507fbbbfc7234d80d_Neg.png 1080w"
+								width="150" sizes="150px" alt></a>
+						<p class="texte-footer">Notre métier, c'est vous !</p>
+						<div class="social-links"><a href="https://linkedin.com" target="_blank"
+								class="social-link w-inline-block"><img
+									src="../fonts/6075b1c658d0be459e0e2005_Group%205.svg" width="50" alt
+									class="social-link-icon"></a></div>
+					</div>
+					<div class="w-layout-grid footer-menus-grid">
+						<div>
+							<h6 class="footer-menu-heading">Pages</h6>
+							<div class="w-layout-grid vertical-link-grid"><a href="../a-propos/a-propos.html" class="hover-link">À
+									propos</a><a href="../services/services.html" class="hover-link">Services</a><a href="../produits/produits.html"
+									class="hover-link">Produits</a><a href="../certifications/certifications.html"
+									class="hover-link">Certifications</a></div>
+						</div>
+						<div id="w-node-_2328506f-4515-4b6a-7506-5187288521a6-28852186">
+							<h6 class="footer-menu-heading">Contact</h6><a href="nous-joindre.html" aria-current="page"
+								class="button small w-inline-block w--current">
+								<div class="text-block-8">NOUS JOINDRE</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="footer-bottom-contents">
+					<div class="small-text">© 2021 Laboratoire Provence</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="loading-container"><img src="../fonts/6074583e03881b1261808656_loader.svg" loading="eager" alt
+			class="loader"></div>
+	<div class="search-modal">
+		<div class="search-modal-screen"></div>
+		<div class="search-modal-container">
+			<form action="/search" class="horizontal-form search-form w-form"><input type="search"
+					class="form-input w-input" maxlength="256" name="query" placeholder="Search…" id="search"
+					required><input type="submit" value="Search" class="button adjacent-to-input w-button"></form>
+		</div>
+	</div>
+	<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=6074583e03881b082780857b"
+		type="text/javascript" crossorigin="anonymous"></script>
+	<script src="../js/laboratoire-provence.72a880eda.js" type="text/javascript"></script>
+	<!--[if lte IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->
+	<script type="text/javascript" src="../js/weglot.min.js"></script>
+	<script>
+		Weglot.initialize({
+			api_key: 'wg_76dc6e529857d68073baa64d5f8ef5125'
+		});
+	</script>
+
+	<script>
+		// on Weglot init
+		Weglot.on('initialized', () => {
+			// get the current active language
+			const currentLang = Weglot.getCurrentLang();
+			// call updateDropdownLinks function
+			updateSW5DropdownLinks(currentLang);
+		});
+
+		// for each of the .wg-element-wrapper language links
+		document.querySelectorAll('.wg-element-wrapper.sw5 [lang]').forEach((link) => {
+			// add a click event listener
+			link.addEventListener('click', function (e) {
+				// prevent default
+				e.preventDefault();
+				// switch to the current active language      
+				Weglot.switchTo(this.getAttribute('lang'));
+				// call updateDropdownLinks function
+				updateSW5DropdownLinks(this.getAttribute('lang'));
+			});
+		});
+
+		// updateDropdownLinks function
+		function updateSW5DropdownLinks(currentLang) {
+			// get the wrapper element
+			const $wrapper = document.querySelector('.wg-element-wrapper.sw5');
+			// if the .w-dropdown-toggle is not the current active language
+			if ($wrapper.querySelector('.w-dropdown-toggle').getAttribute('lang') !== currentLang) {
+				// get the current active language link
+				const $activeLangLink = $wrapper.querySelector('[lang=' + currentLang + ']');
+				// swap the dropdown toggle's text with the current active language link text
+				const $toggle = $activeLangLink.closest('.wg-element-wrapper').querySelector('.w-dropdown-toggle');
+				const toggleTxt = $toggle.textContent;
+				const activeLangLinkTxt = $activeLangLink.textContent;
+				$toggle.querySelector('div').textContent = activeLangLinkTxt;
+				$activeLangLink.textContent = toggleTxt;
+				// swap the dropdown toggle's lang attr with the current active language link lang attr  
+				const lang = $activeLangLink.getAttribute('lang');
+				const toggleLang = $toggle.getAttribute('lang');
+				$toggle.setAttribute('lang', lang);
+				$activeLangLink.setAttribute('lang', toggleLang);
+			}
+		}
+	</script>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+

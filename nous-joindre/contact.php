192e87503a8f7e2488dd3722da6c3a6bc1ef3637
DanielTitan <118362748+cooL8989@users.noreply.github.com>
2023-05-24 21:23:34 +0100

    Add files via upload

--- nous-joindre/contact.php
+++ nous-joindre/contact.php
@@ -1,1 +1,18 @@
+<?php
+//get data from form  
+$name = $_POST['Name'];
+$company = $_POST['Company'];
+$email = $_POST['Email'];
+$phone = $_POST['Phone'];
+$message = $_POST['Message'];
+$to = "voundiasseng@gmail.com";
+$subject = "Mail From TMS";
 
+$txt ="Name = ". $name . "\r\n  Company = " . $company . "\r\n Email =" . $email . "\r\n Phone =" . $phone . "\r\n Message =" . $message;
+$headers = "From: voundiasseng@gmail.com" . "\r\n" ."CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+//header("Location:index.html");
+?>
